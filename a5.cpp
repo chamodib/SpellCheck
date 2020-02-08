0e9990509e78147cce07bc78fb9624012a459583
chamodib <46634954+chamodib@users.noreply.github.com>
2020-02-07 18:35:26 -0800

    Update a5.cpp

--- a5.cpp
+++ a5.cpp
@@ -1,31 +1,7 @@
 // a5.cpp
 
-/////////////////////////////////////////////////////////////////////////
-//
-// Student Info
-// ------------
-//
 // Name : Chamodi Basnayake
-//
-// Statement of Originality
-// ------------------------
-//
-// All the code and comments below are my own original work. For any non-
-// original work, I have provided citations in the comments with enough
-// detail so that someone can see the exact source and extent of the
-// borrowed work.
-//
-// In addition, I have not shared this work with anyone else, and I have
-// not seen solutions from other students, tutors, websites, books,
-// etc.
-//
-/////////////////////////////////////////////////////////////////////////
 
-//
-// You are only allowed to include these files --- don't include anything else!
-// If you think it's necessary to include another file, please speak to the
-// instructor.
-//
 
 #include "cmpt_error.h"
 #include <iostream>
